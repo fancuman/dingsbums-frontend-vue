a6056df4e9fff4361cbfdfe96f6998e826b344c4
fancuman <fancuman@gmail.com>
2021-04-13 17:14:05 +1000

    add hint message when login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,31 +1,47 @@
 <template>
-  <v-form ref="form" v-model="valid" lazy-validation>
-    <v-text-field
-      v-model="name"
-      :counter="10"
-      :rules="nameRules"
-      label="UserName"
-      required
-    ></v-text-field>
-    <v-text-field
-      v-model="password"
-      :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
-      :rules="[passwordRules.required, passwordRules.min]"
-      :type="showPass ? 'text' : 'password'"
-      name="input-10-1"
-      label="Normal with hint text"
-      hint="At least 8 characters"
-      counter
-      @click:append="showPass = !showPass"
-    ></v-text-field>
-    <v-btn :disabled="!valid" color="success" class="mr-4" @click="loginHandle">
-      Login
-    </v-btn>
-    <router-link to="/register" tag="button">
-      <v-btn color="success" class="mr-4"> Register </v-btn>
-    </router-link>
-    <v-btn color="error" class="mr-4" @click="reset"> Reset </v-btn>
-  </v-form>
+  <div>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-text-field
+        v-model="name"
+        :counter="10"
+        :rules="nameRules"
+        label="UserName"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="password"
+        :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+        :rules="[passwordRules.required, passwordRules.min]"
+        :type="showPass ? 'text' : 'password'"
+        name="input-10-1"
+        label="Normal with hint text"
+        hint="At least 8 characters"
+        counter
+        @click:append="showPass = !showPass"
+      ></v-text-field>
+      <v-btn
+        :disabled="!valid"
+        color="success"
+        class="mr-4"
+        @click="loginHandle"
+      >
+        Login
+      </v-btn>
+      <router-link to="/register" tag="button">
+        <v-btn color="success" class="mr-4"> Register </v-btn>
+      </router-link>
+      <v-btn color="error" class="mr-4" @click="reset"> Reset </v-btn>
+    </v-form>
+    <v-snackbar v-model="snackbar" :timeout="timeout">
+      {{ hinttext }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn color="blue" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
 </template>
 
 <script>
@@ -46,6 +62,9 @@ export default {
       required: (value) => !!value || "Required.",
       min: (v) => v.length >= 8 || "Min 8 characters",
     },
+    hinttext: "",
+    snackbar: false,
+    timeout: 2000,
   }),
 
   methods: {
@@ -60,6 +79,12 @@ export default {
           password: this.password,
         });
         console.log("result:", result);
+
+        if (result.code == 0) 
+          this.hinttext="Login failed!"
+        else
+          this.hinttext="Login success!"
+        this.snackbar = true;
       } catch (error) {
         console.log("error:", error);
       }
